aa4c74bfedfc25095e1c67f445bf41da61e2af7b
zdm <zdm@softvisio.net>
2024-08-26 11:47:13 +0300

    feat: store content option

--- bin/install.js
+++ bin/install.js
@@ -2,8 +2,16 @@
 
 import childProcess from "node:child_process";
 
+// npm install --platform=linux --arch=x64 sharp
+
+// (node install/libvips && node install/dll-copy && prebuild-install) || (node install/can-compile && node-gyp rebuild && node install/dll-copy)
+
 if ( process.platform === "win32" ) {
-    const res = childProcess.spawnSync();
+    const res = childProcess.spawnSync( "node install/libvips && node install/dll-copy && prebuild-install", null, {
+        "shell": true,
+        "cwd": "node_modules/sharp",
+        "stdio": "inherit",
+    } );
 
     process.exit( res.status );
 }
